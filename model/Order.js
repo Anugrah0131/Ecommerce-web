ed2cd39c32fec210e893140a5be380a894ce7c0e
Anugrahh <anugrah4840@gmail.com>
2025-12-06 22:00:48 +0530

    new

---
+++ model/Order.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: { type: String, default: "guest" },
+
+    shipping: {
+      fullName: String,
+      phone: String,
+      address: String,
+      city: String,
+      state: String,
+      pincode: String,
+    },
+
+    items: [
+      {
+        productId: String,
+        title: String,
+        price: Number,
+        quantity: Number,
+        image: String,
+      },
+    ],
+
+    paymentMethod: { type: String, default: "cod" },
+
+    amount: Number,
+
+    status: {
+      type: String, 
+      enum: ["pending", "confirmed", "shipped", "delivered"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Order", OrderSchema);
